b76ca8f988001fd9dd229177e53c1339e5712cae
Elavarasan Siluvaimanickam <elavarasan.s@ebmpapst.local>
2022-12-22 21:10:33 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/production-sub-path/" : "/",
+  publicPath: "/people-list/",
 });
